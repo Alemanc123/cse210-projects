71f192ba50db9f0f1564fe3b086b9d0f705dc8cc
Alemanc123 <theminiband@hotmail.com>
2024-11-21 03:09:46 -0600

    Prep 1 working

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastname}, {name} {lastname}.");
     }
+
 }

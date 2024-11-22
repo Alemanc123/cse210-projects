0871dcd132d649ff03f3b0cd18512e22bce20786
Alemanc123 <theminiband@hotmail.com>
2024-11-22 02:55:16 -0600

    Core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageInt = int.Parse(percentage);
+
+
+        string letter = "";
+    
+
+        if (percentageInt >= 90)
+        { 
+            letter = "A";
+            
+        } 
+        
+        else if (percentageInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (letter == "D" || letter == "F")
+
+        {
+            Console.Write( $"Your letter grade is: {letter}, better luck next time.");
+        }
+        else
+        {
+            Console.Write( $"Your letter grade is: {letter}, congratulations! you passed the course.");
+        }
+
+    }   
+
+
+
+
+
+
 }

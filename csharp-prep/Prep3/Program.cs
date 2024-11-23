2bc92eb5a3b79a21ba9661e3f66cc799c1c695ea
Alemanc123 <theminiband@hotmail.com>
2024-11-23 00:46:39 -0600

    Program with the randomizer working

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //when the user is aked for the number
+        //Console.Write("What is your magic number? ");
+        //string magicNumber = Console.ReadLine(); 
+        ///Console.Write( "What is your guess? ");
+        //string guess = Console.ReadLine();
+        //int magicNumberInt = int.Parse(magicNumber);
+        //nt guessInt = int.Parse(guess);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 50);
+        Console.WriteLine("Find a number between 1 and 50");
+        
+        
+        int guess = -1;
+
+        while (magicNumber != guess )
+        {
+            Console.Write("What is your guess? ");
+
+            guess = int.Parse(Console.ReadLine()); 
+            
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");    
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        } 
+            
+        }
+
+
+
+
+
+        
+
+
     }
 }
